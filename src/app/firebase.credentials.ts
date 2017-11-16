64f5bf2cd39f472bf9aec383efbf5c61b4c330d1
Eriel Miquilino <desenvolvimento1@hsiautomacao.com.br>
2017-11-16 01:28:52 -0200

    teste

--- src/app/firebase.credentials.ts
+++ src/app/firebase.credentials.ts
@@ -3,7 +3,7 @@ export const FIREBASE_CREDENTIALS ={
   authDomain: "you-can-project.firebaseapp.com",
   databaseURL: "https://you-can-project.firebaseio.com",
   projectId: "you-can-project",
-  storageBucket: "",
+  storageBucket: "you-can-project.appspot.com",
   messagingSenderId: "941867890240"
 };
 
